cf3e5af366efdb524251ba7ec5b5e1c3109b1255
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-08-24 16:50:53 -0300

    🐛 fix(schema): fixed userId error

---
+++ prisma/migrations/20230824193002_update_post_id/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_id_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
