d7de92185f0fdc89de4b0b287747682bbc049442
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-08-28 15:37:46 -0300

    🎨 style(Input): modified component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,11 +6,10 @@ export function Input() {
       <Search
         className='absolute left-3 top-[33%]'
         size={16}
-        color='#fff'
       />
 
       <input
-        className='pl-10 pr-5 py-3 bg-dark-transparent w-full text-gray-200 rounded-md outline-none'
+        className='pl-10 pr-5 py-3 bg-zinc-200 w-full rounded-md outline-none'
         placeholder='Pesquisar no Blog'
       />
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,6 @@ const config: Config = {
         'header-dark-blue': '#574ae8',
         'header-light-blue': '#3ea1db',
       },
-      backgroundColor: {
-        'dark-transparent': '#ffffff33',
-      },
     },
   },
   plugins: [],
