4c4f29dc19da6b44a53d67638a2a6e767b49086b
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-29 12:22:01 -0300

    deploy: changed route return

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,15 +2,7 @@ import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const posts = await prisma.post.findMany({
-    select: {
-      content: true,
-      id: true,
-      title: true,
-      createdAt: true,
-      createdBy: true,
-    },
-  });
+  const posts = await prisma.post.findMany();
 
   if (!posts) {
     return new NextResponse(
