c9df42238ee030d4b48dce00594fc5ed0d1b58f8
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-29 12:17:55 -0300

    deploy: prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient;
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export { prisma };
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
