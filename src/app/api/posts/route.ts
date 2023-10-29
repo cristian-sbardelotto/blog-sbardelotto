efe2c4ae89a13b975e4a9d143c1e3374a782e9e9
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-29 12:25:54 -0300

    deploy: including user data

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,7 +2,11 @@ import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    include: {
+      createdBy: true,
+    },
+  });
 
   if (!posts) {
     return new NextResponse(
